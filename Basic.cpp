bfe4615c03adcd9280462bb6dfa7a55f72a19d9d
Altaf Alam <102013452+Altafalam3@users.noreply.github.com>
2022-12-13 19:26:01 +0530

    Update Basic.cpp

--- Basic.cpp
+++ Basic.cpp
@@ -1,13 +1,14 @@
-#include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
 
 #define int long long int
 
-int32_t main() {
+int32_t main() 
+{
 	int t;
 	cin>>t;
-	while(t--){
+	while(t--)
+	{
 	    
       
 	}
